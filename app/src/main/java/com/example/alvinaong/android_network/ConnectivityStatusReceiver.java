d09e57e6d535d299744b568574b42df2ed1885ce
siyingpoof <alvina.ongg@gmail.com>
2020-06-18 12:47:28 +0800

    refactor notifications

--- app/src/main/java/com/example/alvinaong/android_network/ConnectivityStatusReceiver.java
+++ app/src/main/java/com/example/alvinaong/android_network/ConnectivityStatusReceiver.java
@@ -20,26 +20,40 @@ public class ConnectivityStatusReceiver extends BroadcastReceiver {
     static final String INTENT_ACTION = ConnectivityManager.CONNECTIVITY_ACTION;
     static final String CHANNEL_ID = "network";
 
+    enum Status {
+        CONNECTED,
+        DISCONNECTED
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "onReceive");
         if (!isConnected(context)) {
             Toast.makeText(context, "Disconnected", Toast.LENGTH_LONG).show();
-
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "network notification", NotificationManager.IMPORTANCE_HIGH);
-            notificationChannel.setDescription("network change notification");
-            notificationChannel.enableVibration(true);
-            notificationManager.createNotificationChannel(notificationChannel);
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
-            builder.setSmallIcon(R.mipmap.ic_launcher);
-            builder.setContentTitle("Your network status");
-            builder.setContentText("Disconnected");
-            notificationManager.notify(1, builder.build());
+            createNotification(context, Status.DISCONNECTED);
         } else {
             Toast.makeText(context, "Connected", Toast.LENGTH_LONG).show();
+            createNotification(context, Status.CONNECTED);
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static void createNotification(Context context, Status status) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "network notification", NotificationManager.IMPORTANCE_DEFAULT);
+        notificationChannel.setDescription("network change notification");
+        notificationChannel.enableVibration(true);
+        notificationManager.createNotificationChannel(notificationChannel);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Your network status");
+        if (status.equals(Status.CONNECTED)) {
+            builder.setContentText("Connected");
+        } else {
+            builder.setContentText("Disconnected");
+        }
+        notificationManager.notify(1, builder.build());
     }
 
     public static boolean isConnected(Context context) {
